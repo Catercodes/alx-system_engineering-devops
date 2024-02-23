392d8537d51bf37370672c32fba789cf7b27bfc7
Catercodes <chrisadamsadewuni@gmail.com>
2024-02-24 00:00:54 +0100

    Tasks

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create a file in /tmp with school, og is www-data and permission is 0744
+
+file { '/tmp/school':
+  ensure  => 'file',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
